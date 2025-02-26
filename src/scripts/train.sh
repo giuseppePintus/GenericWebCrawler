5a04d7886df10e46ac0425a21c2386e7a4d1adc0
Pintus <pintus@unibo.it>
2025-02-26 07:29:33 +0100

    a

---
+++ src/scripts/train.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Navigate to your project directory (if needed)
+# cd /path/to/your/project
+
+# Run the testing container and execute the Python script
+docker-compose run --rm \
+    -v ${PWD}/config:/app/config:ro \
+    -v ${PWD}/data:/app/data \
+    testing \
+    python /app/src/notebook/testing_hallucination_detection.py
